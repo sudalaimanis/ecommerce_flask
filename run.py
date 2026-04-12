e5e2d9fd95d0f29e7fbce700069702792e654561
sudalaimani <93456174+sudalaimanis@users.noreply.github.com>
2026-04-12 20:12:39 +0530

    Change app host to '0.0.0.0' for external access

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         seed_if_empty()
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
